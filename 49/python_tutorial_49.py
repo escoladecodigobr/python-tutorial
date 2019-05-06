0452709444f8875e7fbce2b30837a0f9271faa85
Patrick Espake <patrick@envixo.com>
2019-05-06 17:59:45 +0100

    Código Python Tutorial #49

---
+++ 49/python_tutorial_49.py
@@ -0,0 +1,4 @@
+cores1 = ["azul", "verde", "amarelo"]
+cores2 = ["laranja", "rosa", "cinza"]
+cores3 = cores1 + cores2
+print(cores3)
---
+++ 49/readme.md
@@ -0,0 +1,3 @@
+# Como Combinar 2 Listas / Array (Python Tutorial #49)
+
+Link do vídeo: https://youtu.be/tTQ4HvQtikw
